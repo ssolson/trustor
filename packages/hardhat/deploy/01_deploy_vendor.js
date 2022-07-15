a17e1fc976613e0313a2b6dc7579a6c881f798b1
ssolson <ssolson@sandia.gov>
2022-07-14 21:11:19 -0600

    Updated deployment

--- packages/hardhat/deploy/01_deploy_vendor.js
+++ packages/hardhat/deploy/01_deploy_vendor.js
@@ -20,20 +20,26 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const usdc = await ethers.getContract("USDC", deployer);
   console.log("USDC: ", usdc.address);
 
+  // Input arguments Scaffold Eth  
+  const Grantor = "0x69dA48Df7177bc57639F1015E3B9a00f96f7c1d1";
+  const Trustee = "0x1Bd59929EAb8F689B3c384420f4C50A343110E40";
+  const Beneficiary = ["0x1Bd59929EAb8F689B3c384420f4C50A343110E40", 
+                       "0x79864b719729599a4695f62ad22AD488AB290e58"];
 
-  // Input arguments
+  // // Input arguments Metamask 
   //  const Grantor = "0x79864b719729599a4695f62ad22AD488AB290e58";
-  //  const Trustee = "0x79864b719729599a4695f62ad22AD488AB290e58";
+  //  const Trustee = "0xc6bEcBBB10327e57c0083A3127e5F54aB3D2Af23";
   //  const Beneficiary = ["0xc6bEcBBB10327e57c0083A3127e5F54aB3D2Af23", 
   //                       "0x619cCf252351dAAb7cc9E239538c26e942E9430f"];
-  //  const Percentages = [75,25];
-  // Combine inputs to pass to constructor
-  // let argz = [Grantor, Trustee, Beneficiary, Percentages]
-
-   [Grantor, Trustee, Beneficiary1, Beneficiary2, Beneficiary3] = await ethers.getSigners();
-   const Beneficiary = [Beneficiary1.address, Beneficiary2.address];
    const Percentages = [75,25];
-   let argz = [Grantor.address, Trustee.address, Beneficiary, Percentages]
+  // Combine inputs to pass to constructor
+  let argz = [Grantor, Trustee, Beneficiary, Percentages]
+
+  // To match tests on deployment
+  // [Grantor, Trustee, Beneficiary1, Beneficiary2, Beneficiary3] = await ethers.getSigners();
+  // const Beneficiary = [Beneficiary1.address, Beneficiary2.address];
+  // const Percentages = [75,25];
+  // let argz = [Grantor.address, Trustee.address, Beneficiary, Percentages]
 
 
   const deploySimpleT = await deploy("SimpleT", {
