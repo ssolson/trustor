8f31f530722c4c99504ee62451013927531ed60d
ssolson <ssolson@sandia.gov>
2022-12-13 20:31:27 -0700

    Updated with tests

--- packages/hardhat/contracts/Beneficiary.sol
+++ packages/hardhat/contracts/Beneficiary.sol
@@ -7,14 +7,21 @@ import "./Trustee.sol";
 /// @author sters.eth
 /// @notice Contract will has functions for Beneficiaries
 contract Beneficiary is Trustee {
-    event BeneficiaryAdded(address account, uint256 shares);
+    event BeneficiaryAdded(address sender, address account, uint256 shares);
+    event BeneficiaryUpdated(address sender, address account, uint256 shares);
+    event BeneficiaryUpdatedProRata(
+        address sender, 
+        address removed,  
+        uint256 shares_redistributed        
+    );
+    event BeneficiaryRemoved(address sender, address beneficiary, uint256 shares);
     event AssetsReleased(address to, uint256 amount);
     event AssetsReceived(address from, uint256 amount);
 
-    uint256 private _totalShares;
+    uint256 public totalShares;
 
-    mapping(address => uint256) public _shares;
-    mapping(address => uint256) private _released;
+    mapping(address => uint256) public beneficiaryShares;
+    mapping(address => bool) public beneficiaryHasClaimed;
     address[] public beneficiaries;
 
     /// @dev returns array of addresses with beneficiaries
@@ -25,8 +32,9 @@ contract Beneficiary is Trustee {
 
     /// @dev returns array of addresses with beneficiaries shares
     function getAddressShares(address _addr) external view returns (uint256) {
-        return _shares[_addr];
+        return beneficiaryShares[_addr];
     }
+
 
     /**
      * @dev Trustee will finalize beneficiary addresses.
@@ -35,9 +43,9 @@ contract Beneficiary is Trustee {
      */
     function setBeneficiaries(address[] memory _beneficiaries, uint256[] memory shares_)
         public
+        // isState(TrustStates.Active)
         onlyRole(INITIAL_TRUSTEE_ROLE) {
-        // require(checkin is open);
-
+        
         require(
             _beneficiaries.length > 0,
             "Beneficiary: At least 1 beneficiary must be specified."
@@ -45,15 +53,14 @@ contract Beneficiary is Trustee {
 
         require(
             _beneficiaries.length == shares_.length,
-            "Beneficiary: beneficiaries and shares mush have equal length."
+            "Beneficiary: beneficiaries and shares must have equal length."
         );
-
-        _resetBeneficiaries();
 
         for (uint256 i = 0; i < _beneficiaries.length; i++) {
             _addBeneficiary(_beneficiaries[i], shares_[i]);
         }
     }
+
 
     /**
      * @dev Add a new payee to the contract.
@@ -62,28 +69,90 @@ contract Beneficiary is Trustee {
      */
     function _addBeneficiary(address account, uint256 shares_) private {
         require(account != address(0), "Beneficiary: account is the zero address");
-        require(shares_ > 0, "Beneficiary: shares are 0");
-        require(_shares[account] == 0, "Beneficiary: account already has shares");
+        require(shares_ > 0, "Beneficiary: shares must be greater than 0");
 
-        beneficiaries.push(account);
-        grantRole(BENEFICIARY_ROLE, account);
-        _shares[account] = shares_;
-        _totalShares = _totalShares + shares_;
-        emit BeneficiaryAdded(account, shares_);
+        uint256 currentbeneficiaryShares = beneficiaryShares[account];
+        beneficiaryShares[account] = shares_;
+        if (currentbeneficiaryShares == 0 ) {
+            beneficiaries.push(account);
+            grantRole(BENEFICIARY_ROLE, account);
+            totalShares = totalShares + shares_;
+            emit BeneficiaryAdded(_msgSender(),account, shares_);
+
+        }
+        else {
+            totalShares = totalShares - currentbeneficiaryShares + shares_;
+            emit BeneficiaryUpdated(_msgSender(),account, shares_);
+        }
+
     }
 
+
     /**
-     * @dev Reset the beneficiaries
+     * @dev remove a beneficiary.
+     * Removing a beneficiary
+     * @param beneficiaryAddress address of beneficiary to remove.
      */
-    function _resetBeneficiaries() private {
-        address account;
-        for (uint256 i = 0; i < beneficiaries.length; i++) {
-            account = beneficiaries[i];
-            revokeRole(BENEFICIARY_ROLE, account);
-            _shares[account] = 0;
+    function _removeBeneficiary(address beneficiaryAddress) internal {
+        require(beneficiaries.length>1, "Cannot remove last beneficiary. Please add replacement before removal.");
+
+        uint256 previousShares = beneficiaryShares[beneficiaryAddress];
+        for (
+            uint256 idx = 0;
+            idx < beneficiaries.length;
+            idx++
+        ) {
+            if(beneficiaryAddress == beneficiaries[idx]){
+                beneficiaries[idx] = beneficiaries[beneficiaries.length - 1];
+                beneficiaries.pop(); 
+                beneficiaryShares[beneficiaryAddress]=0;                
+                _revokeRole(BENEFICIARY_ROLE, beneficiaryAddress);                 
+                emit BeneficiaryRemoved(_msgSender(), beneficiaryAddress, previousShares);
+            }
         }
-        delete beneficiaries;
-        _totalShares = 0;
+    }
+
+
+    function adminRemoveBeneficiary(address beneficiaryAddress) external onlyInitialTrustee {
+        _removeBeneficiary(beneficiaryAddress);
+    }
+
+    /** Internal because active trustee may only remove via designated method. E.g. proRata, perStirpes */
+    function activeTrusteeRemoveBeneficiary(address beneficiaryAddress) internal onlyActiveTrustee {
+        _removeBeneficiary(beneficiaryAddress);
+    }
+
+
+    function beneficiaryDeceasedProRata(address beneficiary) 
+        external 
+        onlyActiveTrustee 
+        isDistribution(DistributionTypes.proRata) {
+        require(findIsABeneficiary(beneficiary), "Passed address is not a current beneficiary" );
+        
+        uint256 sharesToRedistribute = beneficiaryShares[beneficiary];
+        uint256 NToRedistribute = beneficiaries.length - 1;
+        uint256 sharesPerBeneficiary = sharesToRedistribute/NToRedistribute;
+
+        activeTrusteeRemoveBeneficiary(beneficiary);
+
+        for (uint i=0; i < beneficiaries.length; i++) {
+            beneficiaryShares[beneficiaries[i]]+=sharesPerBeneficiary;
+        }   
+
+        emit BeneficiaryUpdatedProRata(
+            _msgSender(), 
+            beneficiary,  
+            sharesToRedistribute
+        );
+    }
+
+    // function beneficiaryDeceasedPerStirpes (address beneficiary, address[] newBeneficiaries) external onlyActiveTrustee {
+    //     // require(isBeneficiary(beneficiary), "Passed address is not a current beneficiary" );
+
+    // }
+        /// @dev returns the number of grantors
+    function getBeneficiariesLength() public view returns (uint256) {
+        return beneficiaries.length;
     }
 
     /// @dev iterates over grantors array to find passed address
@@ -97,17 +166,4 @@ contract Beneficiary is Trustee {
         }        
         return isABeneficiary;
     }
-
-    function beneficiaryDeceasedProRata(address beneficiary) 
-        external 
-        onlyActiveTrustee 
-        isDistribution(DistributionTypes.proRata) {
-        require(findIsABeneficiary(beneficiary), "Passed address is not a current beneficiary" );
-
-    }
-
-    // function beneficiaryDeceasedPerStirpes (address beneficiary, address[] newBeneficiaries) external onlyActiveTrustee {
-    //     // require(isBeneficiary(beneficiary), "Passed address is not a current beneficiary" );
-
-    // }
 }
